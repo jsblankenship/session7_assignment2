367789b878247264a109a1196a2c7399d69374c5
Jeff Blankenship <jsblankenship44@gmail.com>
2017-04-12 14:34:56 -0400

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+function add ( a, b, c, d, e, f, g, h, i, j ) { 
+                     console.log( "sum of 1,2,3 is " , a + b + c );
+					 console.log ("sum of 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 is " , a + b + c + d + e + f + g + h + i + j);
+                };
+				
+			add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
